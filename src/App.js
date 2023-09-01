0c43e550a0e07abb4de442b9c17dea0a0bf3062b
henih <86572062+henih@users.noreply.github.com>
2023-09-01 18:37:22 +0200

    Created initial List functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [list, setList] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(list);
+  }
+
+  function updateList(event) {
+    setList(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-5 App text-center">
+      <h1>🚀 Achieve anything</h1>
+      <form onSubmit={handleSubmit} className="">
+        <input
+          className="m-3 w-50 p-2"
+          type="text"
+          placeholder="What tasks do I have to achieve?"
+          onChange={updateList}
+        ></input>
+        <input type="submit" value="Add" className="p-2"></input>
+      </form>
     </div>
   );
 }
-
-export default App;
