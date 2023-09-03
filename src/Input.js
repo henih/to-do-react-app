cc906ea3afa2e7181b10c3ffdb39e339b238172e
henih <86572062+henih@users.noreply.github.com>
2023-09-03 19:30:46 +0200

    Rendered List Component

--- src/Input.js
+++ src/Input.js
@@ -2,28 +2,34 @@ import { useState } from "react";
 import List from "./List.js";
 
 export default function Input() {
-  const [list, setList] = useState("");
+  const [list, setList] = useState([]); //List of tasks should be an array
+  const [task, setTask] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert(list);
+    setList([...list, task]); // When the form is submitted, the current task is added to list.
+    setTask("");
+    console.log(task);
   }
 
-  function updateList(event) {
-    setList(event.target.value);
+  function updateTask(event) {
+    setTask(event.target.value);
   }
   return (
-    <form onSubmit={handleSubmit} className="">
-      <input
-        className="m-3 w-50 p-2"
-        type="text"
-        placeholder="What tasks do I have to achieve?"
-        onChange={updateList}
-      ></input>
-      <input type="submit" value="Add" className="p-2"></input>
-    </form>
-    <div className="List">
-     <List list={list} />
-     </div>
+    <div>
+      <form onSubmit={handleSubmit} className="">
+        <input
+          className="m-3 w-50 p-2"
+          type="text"
+          placeholder="What tasks do I have to achieve?"
+          onChange={updateTask}
+          value={task}
+        ></input>
+        <input type="submit" value="Add" className="p-2"></input>
+      </form>
+      <div className="List">
+        <List list={list} />
+      </div>
+    </div>
   );
 }
--- src/List.js
+++ src/List.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-export default function List(value) {
-  return "Hello from List";
+export default function List(props) {
+  console.log(props.list);
+  if (props.list.length <= 0) {
+    return <div></div>;
+  } else {
+    return (
+      <ul>
+        {props.list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    );
+  }
 }
+
