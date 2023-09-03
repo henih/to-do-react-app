3e0da9867b673813a695123077ff3dd74f0ca91e
henih <86572062+henih@users.noreply.github.com>
2023-09-03 19:59:13 +0200

    Rendered Completed Task section

--- src/List.js
+++ src/List.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./List.css";
 
 export default function List(props) {
   console.log(props.list);
@@ -6,12 +7,36 @@ export default function List(props) {
     return <div></div>;
   } else {
     return (
-      <ul>
-        {props.list.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <div>
+        <div className="task-overview">
+          <div>
+            <h2>To do</h2>
+            <div className="to-do-list">
+              <ul>
+                {props.list.map((item, index) => (
+                  <li key={index}>
+                    {item}
+                    <button className="btn btn-success m-2">
+                      Mark as Complete
+                    </button>
+                    <button className="btn btn-secondary m-2">Modify</button>
+                    <button className="btn btn-danger m-2">Delete</button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div>
+            <h2>Completed</h2>
+            <div className="completed-tasks">
+              <ul>
+                <li>test</li>
+                <li>test</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
-
